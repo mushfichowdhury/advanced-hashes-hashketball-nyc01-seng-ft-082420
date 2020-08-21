a1775a1366619695c5884f5ee73ef1f82b664404
Mushfi Chowdhury <chowdhurymushfi@gmail.com>
2020-08-21 13:55:15 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -162,7 +162,6 @@ end
 def team_names 
   game_hash.each do |home_away, team_data|
     binding.pry
-    return team_data[0]
   end
 end
 
@@ -173,6 +172,7 @@ def player_stats(name_of_player)
  game_hash.each do |home_away, team_data|
     team_data[:players].each do |player|
         if player[0] == name_of_player
+          return team_data[:players][name_of_player]
       end
     end
   end
